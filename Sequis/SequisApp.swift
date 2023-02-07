c26ce966c182374264f9a4e058a42b2078b8cf70
Nayanda Haberty <hainayanda@outlook.com>
2023-02-08 06:50:50 +0700

    Initial commit

---
+++ Sequis/SequisApp.swift
@@ -0,0 +1,17 @@
+//
+//  SequisApp.swift
+//  Sequis
+//
+//  Created by Nayanda Haberty on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SequisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
