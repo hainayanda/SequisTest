a639053e901c6df2ced6d86b2e3f5e5c3cbbdd79
Nayanda Haberty <hainayanda@outlook.com>
2023-02-08 08:47:46 +0700

    Added LabeledImageModel

--- Modules/Features/MainFeature/Sources/MainFeature/LabeledImageCard.swift
+++ Modules/Features/MainFeature/Sources/MainFeature/LabeledImageCard.swift
@@ -8,19 +8,40 @@
 import SwiftUI
 import CommonUI
 
+// MARK: LabeledImageModel
+
+class LabeledImageModel: ObservableObject {
+    @Published var image: Image
+    @Published var title: String
+    @Published var content: String
+    
+    init(image: Image, title: String, content: String) {
+        self.image = image
+        self.title = title
+        self.content = content
+    }
+}
+
+// MARK: LabeledImageCard
+
 struct LabeledImageCard: View {
+    
+    @ObservedObject var viewModel: LabeledImageModel
+    
+    init(viewModel: LabeledImageModel) {
+        self.viewModel = viewModel
+    }
     
     var body: some View {
         Card {
             HStack {
-                Image("Test", bundle: .module)
+                viewModel.image
                     .resizable()
                     .frame(width: 100, height: 100)
                     .innerCornerRadius()
-                Spacer()
                 VStack(alignment: .leading) {
-                    Title2("Author")
-                    Text("Amelia Earheart")
+                    Title2(viewModel.title)
+                    Text(viewModel.content)
                     Spacer()
                 }
                 .padding()
@@ -32,8 +53,16 @@ struct LabeledImageCard: View {
     }
 }
 
+// MARK: LabeledImageCard_Previews
+
 struct LabeledImageCard_Previews: PreviewProvider {
     static var previews: some View {
-        LabeledImageCard()
+        LabeledImageCard(
+            viewModel: .init(
+                image: Image("Test", bundle: .module),
+                title: "Author",
+                content: "Amelia Earheart"
+            )
+        )
     }
 }
