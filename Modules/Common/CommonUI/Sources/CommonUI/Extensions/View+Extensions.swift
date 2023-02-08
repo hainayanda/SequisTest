235376760fd2810ce7f60246f974d777f9d96294
Nayanda Haberty <hainayanda@outlook.com>
2023-02-08 08:04:04 +0700

    Add View Extensions

---
+++ Modules/Common/CommonUI/Sources/CommonUI/Extensions/View+Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Nayanda Haberty on 8/2/23.
+//
+
+import Foundation
+import SwiftUI
+
+public extension View {
+    func defaultShadow() -> some View {
+        shadow(color: .shadow, radius: 10)
+    }
+    
+    func defaultCornerRadius() -> some View {
+        cornerRadius(24)
+    }
+    
+    func innerCornerRadius() -> some View {
+        cornerRadius(20)
+    }
+}
