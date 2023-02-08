bc3b38b77fe0bb2be1cd2d269dcc5e4a74039a82
Nayanda Haberty <hainayanda@outlook.com>
2023-02-08 18:15:31 +0700

    Refactor how Detail rendered

--- Modules/Features/DetailFeature/Sources/DetailFeature/DetailView/Component/PostItem.swift
+++ Modules/Features/DetailFeature/Sources/DetailFeature/DetailView/Component/PostItem.swift
@@ -27,16 +27,14 @@ struct PostItem: View {
             VStack(alignment: .leading, spacing: 8) {
                 Title2("Author")
                     .foregroundColor(.appText)
-                Text("Hello")
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
                     .foregroundColor(.appText)
-                Spacer()
                 FootNote("yesterday")
                     .foregroundColor(.gray)
             }
             Spacer()
         }
         .padding()
-        .frame(minHeight: 124)
         .overlay {
             VStack {
                 Spacer()
--- Modules/Features/DetailFeature/Sources/DetailFeature/DetailView/DetailView.swift
+++ Modules/Features/DetailFeature/Sources/DetailFeature/DetailView/DetailView.swift
@@ -8,18 +8,21 @@
 import SwiftUI
 import CommonUI
 
-struct DetailView: View {
-    var body: some View {
+public struct DetailView: View {
+    
+    public var body: some View {
         ScrollView {
             VStack {
                 ImageCompatible(url: "https://zonneveld.dev/wp-content/uploads/2019/10/swiftUI-banner.jpg")
                     .resizable()
-                    .scaledToFill()
+                    .aspectRatio(CGSize(width: 2, height: 1), contentMode: .fill)
                 ForEach(0..<20) { _ in
                     PostItem()
                 }
             }
         }
+        .navigationTitle("Image Detail")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
