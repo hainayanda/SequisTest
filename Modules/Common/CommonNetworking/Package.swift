4c711c969fa517ce5aec2f2a005d3e86c49fafa9
Nayanda Haberty <hainayanda@outlook.com>
2023-02-09 07:57:01 +0700

    Adding a resources

--- Modules/Common/CommonNetworking/Package.swift
+++ Modules/Common/CommonNetworking/Package.swift
@@ -27,7 +27,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CommonNetworking",
-            dependencies: ["CommonUtilities"]),
+            dependencies: ["CommonUtilities"],
+            resources: [.process("Resources")]
+        ),
         .target(
             name: "CommonNetworkingMock",
             dependencies: [
